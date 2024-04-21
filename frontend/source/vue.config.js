c2bb44c6dce2ae02d661b6ab2802100e4146f019
kobebrylant <wojto811@wp.pl>
2024-04-21 22:17:35 +0200

    Add Ip

--- frontend/source/vue.config.js
+++ frontend/source/vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '^/': {
-        target: 'http://52.200.231.173:8080',
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  }
+  transpileDependencies: true
 })
